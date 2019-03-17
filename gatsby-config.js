7b515a7b1e624693fbe2f81175fef1a633f9b82d
Niklas Ravnsborg-Gjertsen <niklasravnsborg@gmail.com>
2019-03-17 21:48:22 +0100

    Team Foto einbauen

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,15 @@ module.exports = {
     'gatsby-plugin-emotion',
     'gatsby-plugin-react-helmet',
     {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/images`,
+        name: 'images'
+      }
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: 'Nachhaltigkeitsfreunde Wilhelmshaven',
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,27 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import Img from 'gatsby-image'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
+import styled from 'react-emotion'
 
-export default _ => (
+const Team = styled.div`
+  float: right;
+  width: 100%;
+  margin-bottom: 20px;
+  @media(min-width: 750px) {
+    max-width: 460px;
+    margin: 0 0 20px 20px;
+  }
+  @media(min-width: 1000px) {
+    max-width: 500px;
+    margin-right: -60px;
+  }
+  border-radius: 4px;
+  overflow: hidden;
+`
+
+export default ({ data })  => (
   <Layout>
     <h1>Wir stehen für Nachhaltigkeit</h1>
     <p>Moin! Wir sind eine junge motivierte Truppe aus Wilhelmshaven. Unser
@@ -12,8 +30,11 @@ Aktionen erreichen, die die Menschen zum Nachdenken anregen und gleichzeitig Spa
 Beispiel Workshops, Kunstaktionen und Diskussionsrunden.
 Besonders liegt uns am Herzen, umweltinteressierte Menschen in der Region miteinander zu
 vernetzen, sowie neue Interessierte ins Boot zu holen.</p>
-    <p>Ausschlaggebender Impuls für unseren Zusammenschluss war ein Videowettbewerb namens „Lass ma’ nachhaltig“, bei dem wir uns bewarben. Von etwa 600 Bewerbern haben wir es unter die 20
-Finalisten geschafft. Und plötzlich war unser Video mit über 10.000 Views viral – für uns Landeier ist das eine echte Hausnummer!
+    <Team>
+      <Img fluid={data.team.childImageSharp.fluid} alt="Richard, Niklas, Francesco, Sophie"/>
+    </Team>
+    <p>Ausschlaggebender Impuls für unseren Zusammenschluss war ein Videowettbewerb namens „Lass ma’ nachhaltig“, bei dem wir uns bewarben. Von etwa 600 Bewerbern <a href="https://youtu.be/wGqCjiW0nVA" target="_blank">haben wir es unter die 20
+Finalisten geschafft</a>. Und plötzlich war unser Video mit über 10.000 Views viral – für uns Landeier ist das eine echte Hausnummer!
 Wenig später meldeten wir uns beim Förderprojekt „Frieslands Helden der Heimat“ an, denn eines
 war sicher: Unabhängig davon, ob wir den Videocontest gewinnen oder nicht, wollen wir das
 Umweltbewusstsein in Wilhelmshaven vorantreiben. Der Ideenfunke war angefacht.
@@ -23,3 +44,15 @@ umweltbewusste Alternativen machen. Zusammen können wir dafür sorgen, dass Wil
 tatsächlich eine „grüne Stadt am Meer“ wird!</p>
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    team: file(absolutePath: { regex: "/team/" }) {
+      childImageSharp {
+        fluid(maxWidth: 500) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
