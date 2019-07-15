5eced673430c01a9d28b90b2124910a88ef8e710
Niklas Ravnsborg-Gjertsen <niklasravnsborg@gmail.com>
2019-07-15 19:32:13 +0200

    Impressum anpassen

--- src/pages/impressum.js
+++ src/pages/impressum.js
@@ -10,33 +10,24 @@ export default _ => (
     <h1>Impressum</h1>
 
     <p>
-      <b>Angaben gemäß § 5 TMG</b><br/>
-      Grüncool<br/>
-      Karl-Harms Str. 18<br/>
-      26419 Schortens
-    </p>
-
-    <p>
-      <b>Vertreten durch</b><br/>
-      Francesco Huber<br/>
-      Niklas Ravnsborg<br/>
-      Richard Keil<br/>
-      Kadija Ege<br/>
-      Luna Hilbers<br/>
-      Sophie Plenio
-    </p>
-
-    <p>
-      <b>Kontakt</b><br/>
-      Telefon: <a href="tel:+4916781704372">+49 167 81704372</a><br/>
-      Email: <a href="mailto:info@gruen.cool">info@gruen.cool</a>
-    </p>
-
-    <p>
       <b>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV</b><br/>
-      Francesco Huber<br/>
+      Grüncool (Francesco Huber)<br/>
       Karl-Harms Str. 18<br/>
-      26419 Schortens
+      26419 Schortens<br/>
+    </p>
+
+    <p>
+      Kontakt: <a href="mailto:info@gruen.cool">info@gruen.cool</a> | <a href="tel:+4916781704372">+49 167 81704372</a>
+    </p>
+
+    <p>
+      <b>Veranstalter am 18. August 2019</b><br/>
+      Neue Botschaft Sued e.V.<br/>
+      Eingetragen im Vereinsregister.<br/>
+      Registergericht: Amtsgericht Oldenburg (Oldenburg)<br/>
+      Registernummer: VR 201561<br/>
+      <br/>
+      Gemeinnützig durch Bescheid nach § 60a Abs. 1 AO über die gesonderte Feststellung der Einhaltung der satzungsmäßigen Voraussetzungen nach §§ 51, 59, 60 und 61 AO
     </p>
 
     <p>
